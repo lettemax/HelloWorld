ffb6c41c98b87c89cd7eddda53a29d41d1f75201
Max Lettenberger <maxlettenberger@gmail.com>
2018-12-19 19:05:40 -0600

    Working with arrays

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,78 @@
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Max");
 
-        Car myCar = new Car(
-                25.5,
-        "1BC32E", java.awt.Color.BLUE, true );
 
-        Car sallyCar = new Car(31.3,
-        "14DR56", java.awt.Color.BLACK,false);
+        // 31, 45, 22, 98, 10
+        int[] numbers = new int[5];
 
-        System.out.println("My car's License Plate: " + myCar.licensePlate);
-        System.out.println("Sally's License Plate: " + sallyCar.licensePlate);
+        numbers[0] = 31;
+        numbers[1] = 45;
+        numbers[2] = 22;
+        numbers[3] = 98;
+        numbers[4] = 10;
 
-        System.out.println(myCar.paintColor);
-        myCar.changePaintColor(java.awt.Color.GREEN);
-        System.out.println(myCar.paintColor);
+        int[] numbers2 = {31, 45, 22, 98, 10};
 
+//        String[] myFavoriteCandyBars = {"Twix", "Hershey's", "Crunch"};
+//
+//        System.out.println("Index 1: " + myFavoriteCandyBars[1]);
+//
+//        myFavoriteCandyBars[2] = "Vegan bar";
+//        System.out.println("Index 2: " + myFavoriteCandyBars[2]);
+//
+//        System.out.println("Length of myFavoriteCandyBars[]: " + myFavoriteCandyBars.length);
+
+        System.out.println(0 < 'a');
+
+
+
+
+
+
+
+
+        //        System.out.println("Enter a word: ");
+
+//        Scanner sc = new Scanner(System.in);
+//        int userNumber = sc.nextInt();
+//        System.out.println(userNumber);
+//        double userNumber2 = sc.nextDouble();
+//        System.out.println(userNumber2);
+
+//        String userInput = sc.next();
+//
+//        String uppercased = userInput.toUpperCase();
+//        System.out.println(userInput);
+//        System.out.println(uppercased);
+//
+//        char firstCharacter = userInput.charAt(0);
+//        System.out.println(firstCharacter);
+//
+//        System.out.println("Contains: " + userInput.contains(("Enter").toLowerCase()));
+
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//        System.out.println("Max");
+//
+//        Car myCar = new Car(
+//                25.5,
+//        "1BC32E", java.awt.Color.BLUE, true );
+//
+//        Car sallyCar = new Car(31.3,
+//        "14DR56", java.awt.Color.BLACK,false);
+//
+//        System.out.println("My car's License Plate: " + myCar.licensePlate);
+//        System.out.println("Sally's License Plate: " + sallyCar.licensePlate);
+//
+//        System.out.println(myCar.paintColor);
+//        myCar.changePaintColor(java.awt.Color.GREEN);
+//        System.out.println(myCar.paintColor);
+//
+//    }
     }
 }
